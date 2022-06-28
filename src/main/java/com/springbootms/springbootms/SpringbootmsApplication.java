3c98fa5f19c6dd3475c26f8b552751413150cdd1
Ashok It <sanjuslrup@gmail.com>
2022-06-28 21:43:31 +0530

    yes

---
+++ src/main/java/com/springbootms/springbootms/SpringbootmsApplication.java
@@ -0,0 +1,14 @@
+package com.springbootms.springbootms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmsApplication.class, args);
+	}
+
+}
+	
